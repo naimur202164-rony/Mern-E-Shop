957195a4c40931959de969699de4079874fcd6c5
naimur <naimur202163@gmail.com>
2022-07-25 13:11:44 +0600

    reviews Schema added

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,5 +1,30 @@
 import mongoose from "mongoose";
 
+const reviewsSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+    },
+    comment: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
 const productSchema = mongoose.Schema(
   {
     user: {
@@ -53,3 +78,7 @@ const productSchema = mongoose.Schema(
     timestamps: true,
   }
 );
+
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
