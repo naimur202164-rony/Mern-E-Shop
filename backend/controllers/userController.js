59ecc2fd32554b3d1b01eb8a10e7470086016b57
naimur <naimur202163@gmail.com>
2022-07-25 16:52:33 +0600

    userProfile added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -52,3 +52,21 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new Error("Invalid user data");
   }
 });
+
+// Get User Profile
+
+const getUserProfile = asyncHander(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
