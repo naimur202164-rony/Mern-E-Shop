bef90ec9730e3a15d68d1520a9d74008868e318b
naimur <naimur202163@gmail.com>
2022-07-25 15:57:37 +0600

    user model is added

---
+++ backend/models/userModel.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+import bcrypt from "bcryptjs";
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
