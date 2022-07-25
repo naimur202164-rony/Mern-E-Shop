71b51040b4fd7cd56a33e509a71abdc515bc0093
naimur <naimur202163@gmail.com>
2022-07-25 14:35:46 +0600

    update product created

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -51,6 +51,52 @@ const deleteProduct = asyncHandler(async (req, res) => {
   }
 });
 
+//
 
+const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: "Sample Name",
+    price: 0,
+    user: req.user._id,
+    image: "/img/sample.jpg",
+    brand: "Sample Brand",
+    category: "Sample Category",
+    countInStock: 0,
+    rating: 0.0,
+    numReviews: 0,
+    description: "Sample Text",
+  });
 
-// 
+  const createdProduct = await product.save();
+
+  res.status(201).json({
+    success: true,
+    message: "Product Created Successfuly",
+    createdProduct,
+  });
+});
+
+// Update Product
+
+const updateProduct = asyncHandler(async (req, res) => {
+  const { name, price, description, image, brand, category, countInStock } =
+    req.body;
+
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    product.name = name;
+    product.price = price;
+    product.description = description;
+    product.image = image;
+    product.brand = brand;
+    product.category = category;
+    product.countInStock = countInStock;
+
+    const updatedProduct = await product.save();
+    res.json(updatedProduct);
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
