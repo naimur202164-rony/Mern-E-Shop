89ecf04a95e5bbd87df4c888c9a808c970cd1b3f
naimur <naimur202163@gmail.com>
2022-07-25 14:21:30 +0600

    product Deleted successfully

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -22,3 +22,35 @@ const getProduct = asyncHandler(async (req, res) => {
   // Throw New Error
   res.json({ products, page, pages: Math.ceil(count / pageSize) });
 });
+
+// Get all products by id
+
+const getProductById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    res.json(product);
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
+
+// Delete Product
+
+const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    await product.remove();
+    res.json({
+      message: "Product Removed Successfully",
+    });
+  } else {
+    res.status(404);
+    throw new Error("Product Not found");
+  }
+});
+
+
+
+// 
