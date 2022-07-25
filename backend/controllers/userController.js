6fa54178f31f92682d905b98ff21dc78f798afd7
naimur <naimur202163@gmail.com>
2022-07-25 17:01:07 +0600

    userUpdate Method added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -72,8 +72,33 @@ const getUserProfile = asyncHander(async (req, res) => {
 });
 
 // GEt ALl Users
-
-const getUsers = asyncHander(async (req, res) => {
+const getUsers = asyncHandler(async (req, res) => {
   const users = await User.find({});
   res.json(users);
+});
+
+// Update User Profile
+
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      token: generateToken(updatedUser._id),
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 });
