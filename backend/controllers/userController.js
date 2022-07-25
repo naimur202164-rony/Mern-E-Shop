ae6f06aa1bd465876503767306f4518e8a99582a
naimur <naimur202163@gmail.com>
2022-07-25 16:42:12 +0600

    authUser added on useControllers

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,7 +2,21 @@ import asyncHander from "express-async-handler";
 import User from "../models/userModel.js";
 import generateToken from "../utils/generateToken.js";
 
-const authUser = asyncHander(async (req, res) => {
+const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
+
   const user = await User.findOne({ email: email });
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401); // Not authorized
+    throw new Error("Invalid Email or Password");
+  }
 });
